b585add27669fed635d490f9554cf9a4adc203fb
andychu666 <ss86314@gmail.com>
2024-05-21 13:27:17 +0800

    Starting a Server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+/*
+* Primary file for the API
+*
+*/
+
+// Dependecies
+var http = require('http');
+//const http = require('http');
+
+// The server should respond to all requests with a string
+var server = http.createServer(function(req,res){
+    res.end('Hello World\n');
+});
+
+// Start the server, and have it listen on port 65535
+server.listen(65535,function(){
+    console.log("The server is listening on port 65535 now");
+});
